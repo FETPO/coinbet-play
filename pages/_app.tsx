92096b4e4477421db1c363ec847d579a22a03494
Stanislav Trenev <stanislav.trenev@gmail.com>
2023-01-20 15:32:58 +0200

    feat: add loading progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import { AppProps } from "next/app";
 import "../styles/globals.scss";
 import { WalletContextWrapper } from "../context/wallet.context";
 import Layout from "../components/layout";
+import NProgress from 'nprogress';
 import { ThemeProvider } from "next-themes";
 import { PolygonScanContextWrapper } from "../context/polygonscan.context";
 import { ContractsContextWrapper } from "../context/contract.context";
 import { AlchemyContextWrapper } from "../context/alchemy.context";
 import { SubgraphContextWrapper } from "../context/subgraph.context";
+import { Router } from "next/router";
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
